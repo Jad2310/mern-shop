2547efa07eddf521da35a1c4028f4d57d08700da
Jad <jadwehbe14@gmail.com>
2022-07-09 00:44:58 +0300

    Slider Component fetch top products

--- Frontend/src/components/Slider.tsx
+++ Frontend/src/components/Slider.tsx
@@ -1,28 +1,47 @@
+import { useEffect } from "react";
 import { Carousel, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import {
+  getTopProducts,
+  resetProduct,
+} from "../features/products/productSlice";
+import Spinner from "./Spinner";
 
 function Slider() {
+  const dispatch = useAppDispatch();
+
+  const productsTopRated = useAppSelector((state) => state.products.top);
+  const { product, isError, isLoading, message } = productsTopRated;
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    dispatch(getTopProducts());
+
+    return () => {
+      dispatch(resetProduct());
+    };
+  }, []);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <Carousel className="bg-dark m-auto mt-4" pause="hover">
-      <Carousel.Item>
-        <Image src="/images/camera.jpg" alt="camera" fluid />
-        <Carousel.Caption className="carousel-caption">
-          <h2>Cannon EOS SOD DSLR CAMERA ($929.99)</h2>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <Image src="/images/phone.jpg" alt="iphone" fluid />
-        <Carousel.Caption className="carousel-caption">
-          <h2>IPHONE 11 PRO 256GB MEMORY ($599.99)</h2>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <Image src="/images/airpods.jpg" alt="airpods" fluid />
-        <Carousel.Caption className="carousel-caption">
-          <h2>AIRPODS WIRELESS BLUETOOTH HEADPHONES ($89.99)</h2>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {product.map((item) => (
+        <Carousel.Item key={item._id}>
+          <Link to={`/product/${item._id}`}>
+            <Image src={item.image} alt={item.name} fluid />
+            <Carousel.Caption className="carousel-caption">
+              <h2>{item.name}</h2>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
