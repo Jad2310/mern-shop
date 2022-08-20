669c621ad55b22d29dd41f3398ed4576d3c5c5fd
Jad <jadwehbe14@gmail.com>
2022-08-20 17:01:17 +0300

    Footer Component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -14,24 +14,26 @@ import { Header } from "./components";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Router>
         <Header />
-        <Routes>
-          <Route path="/">
-            <Route index element={<HomePage />} />
-            <Route path="cart">
-              <Route index element={<CartPage />} />
-              <Route path=":id" element={<CartPage />} />
+        <main className="py-3">
+          <Routes>
+            <Route path="/">
+              <Route index element={<HomePage />} />
+              <Route path="cart">
+                <Route index element={<CartPage />} />
+                <Route path=":id" element={<CartPage />} />
+              </Route>
+              <Route path="product/:id" element={<ProductPage />} />
+              <Route path="login/" element={<LoginPage />} />
+              <Route path="register/" element={<RegisterPage />} />
             </Route>
-            <Route path="product/:id" element={<ProductPage />} />
-            <Route path="login/" element={<LoginPage />} />
-            <Route path="register/" element={<RegisterPage />} />
-          </Route>
-        </Routes>
+          </Routes>
+        </main>
       </Router>
       <ToastContainer />
-    </div>
+    </>
   );
 }
 
